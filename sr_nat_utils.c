2bacddbda8b1f71f95879683af58b09bd98645c1
Gil Shotan <gilsho@cs.stanford.edu>
2012-12-08 10:58:25 -0800

    minor change

--- sr_nat_utils.c
+++ sr_nat_utils.c
@@ -14,7 +14,7 @@ uint16_t tcp_cksum (sr_ip_hdr_t *iphdr, sr_tcp_hdr_t *tcphdr,  unsigned int tcpl
 	data_pseudo_ip->ip_src = iphdr->ip_src;
 	data_pseudo_ip->ip_dst = iphdr->ip_dst;
 	data_pseudo_ip->empty = 0; //just in case
-	data_pseudo_ip->ip_p = tcplen;
+	data_pseudo_ip->ip_p = iphdr->ip_p;
 	data_pseudo_ip->tcp_len = htons(tcplen);
 
 	sr_tcp_hdr_t *data_tcp = (sr_tcp_hdr_t *) (data + sizeof(sr_ip_pseudo_hdr_t));
